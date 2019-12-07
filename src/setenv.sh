411eec8671f29385f912ce5ec8c71a0300008238
mike1chen <58618750+mike1chen@users.noreply.github.com>
2019-12-07 14:02:38 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,7 @@
 export GITACCOUNT=mike1chen
 
 export MYCLUSTER=tektonknativebeijing20
-export KUBECONFIG=
-
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing20/kube-config-tok02-tektonknativebeijing20.yml
 export NAMESPACE=mikechen
 export EMAIL=896612110@qq.com
-export INGRESS=
+export INGRESS=tektonknativebeijing20-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
